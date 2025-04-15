cd83243eebf3d64cf96c9bc78179d14b1b78acc0
leisson <leissonalvarez@gmail.com>
2025-04-14 23:36:01 -0500

    aqui se intentaron cosas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const port = 3000;
-const animeRoutes = require("./routes/anime"); 
+const animeRoutes = require("./routes/anime");
+const authRoutes = require("./routes/authentication");
 const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use("/api", animeRoutes);
+app.use("/api", authRoutes);
 
 mongoose
     .connect(process.env.MONGODB_URI)
--- src/routes/anime.js
+++ src/routes/anime.js
@@ -1,49 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const animeSchema = require("../models/animeModel");
+const verifyToken = require("./validate_token");
+
 
 // Crear nuevo anime
 router.post("/animes", (req, res) => {
-  const anime = new animeSchema(req.body);
-  anime
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+    const anime = new animeSchema(req.body);
+    anime
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
 });
 
 // Obtener todos los animes
-router.get("/animes", (req, res) => {
-  animeSchema
-    .find()
-    .then((data) => res.json(data))
-    .catch((error) => res.status(500).json({ message: error.message }));
+router.get("/animes", verifyToken, (req, res) => {
+    animeSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error.message }));
 });
 
 // Obtener un anime por ID
 router.get("/animes/:id", (req, res) => {
-  const { id } = req.params;
-  animeSchema
-    .findById(id)
-    .then((data) => res.json(data))
-    .catch((error) => res.status(404).json({ message: error.message }));
+    const { id } = req.params;
+    animeSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.status(404).json({ message: error.message }));
 });
 
 // Actualizar anime por ID
 router.put("/animes/:id", (req, res) => {
-  const { id } = req.params;
-  animeSchema
-    .updateOne({ _id: id }, { $set: req.body })
-    .then((data) => res.json(data))
-    .catch((error) => res.status(400).json({ message: error.message }));
+    const { id } = req.params;
+    animeSchema
+        .updateOne({ _id: id }, { $set: req.body })
+        .then((data) => res.json(data))
+        .catch((error) => res.status(400).json({ message: error.message }));
 });
 
 // Eliminar anime por ID
 router.delete("/animes/:id", (req, res) => {
-  const { id } = req.params;
-  animeSchema
-    .deleteOne({ _id: id })
-    .then((data) => res.json(data))
-    .catch((error) => res.status(400).json({ message: error.message }));
+    const { id } = req.params;
+    animeSchema
+        .deleteOne({ _id: id })
+        .then((data) => res.json(data))
+        .catch((error) => res.status(400).json({ message: error.message }));
 });
 
 module.exports = router;
